f74e7b5889441e41d2624856551f22ec24fa3b95
Bruno <Bruno@192.168.107.229>
2017-06-19 21:28:21 -0300

    Inicio Sistema Especialista

---
+++ carros.com/se/Models/Cliente.java
@@ -0,0 +1,31 @@
+package Models;
+
+public class Cliente {
+	
+	private Categoria categoria;
+	private Finalidade finalidade;
+	private NumPassageiro numPassageiro;
+	private RendaMensal rendaMensal;
+	private TipoCombustivel tipoCombustivel;
+	
+	public enum Categoria{
+		
+	}
+	
+	public enum Finalidade{
+		
+	}
+	
+	public enum NumPassageiro{
+		
+	}
+	
+	public enum RendaMensal{
+		
+	}
+	
+	public enum TipoCombustivel{
+		
+	}
+
+}
